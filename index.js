fd7e9a034a595852b825cc00f6c18563dc1cdd44
Carl Dunn <carl.dunn@roq.co.uk>
2023-02-09 10:51:51 +0000

    Added more to the program

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ const prompt = require('prompt-sync')();
 let input = '';
 
 while (input != 'QUIT') {
-    input = prompt('what is your name?: ');
+    input = prompt('What is your name?: ');
     input = input.toUpperCase();
 
-    if (input == 'CARL'){
-        console.log('Welcome ')
+    if (input != 'CARL' && input !='QUIT') {
+        console.log('Welcome Peasant')
     }
+    else if(input != 'QUIT'){
+        console.log('Welcome King')
+    };
+
+    if (input == 'QUIT'){
+        console.log('Goodbye Peasant')
+    };
 
 }
